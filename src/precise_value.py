18f1b7f4bdc2f4cb8f61fe21ed38edfab426701a
ChrisGottsacker <atcgottsacker@gmail.com>
2016-08-16 16:20:19 -0500

    __init__() for precise_value class

--- src/precise_value.py
+++ src/precise_value.py
@@ -1,4 +1,19 @@
+from enum import Enum
+import sys
+
 class precise_value(object):
+    def __init__(self, value):
+        self.rounding_method = rounding_methods.proper
+        self.string_value = value
+        self.decimal_value = Decimal(value)
+
+
+
+    def set_rounding_method(self, rounding_method):
+        if rounding_method in rounding_methods.__members__.keys()
+            self.rounding_method = rounding_method
+        else: sys.exit('Error: unsupported rounding method specified')
+
     def num_decimal_digits(value):
     	'''Counts number of significant digits to right of decimal point'''
     	parsed_value = remove_non_digits(value)
@@ -23,14 +38,17 @@ class precise_value(object):
     	return( re.sub(r'^-*((\d+\.?\d*)|(\d*\.\d+)).*$', r'\1', value) )
 
     def remove_non_significant_zeroes(value):
-    	# remove leading zeroes to left of decimal point, keeping at most 1 zero
-    	# e.g. -03.05E+4 -> 305E+4    or    05 -> 5    or   0.4 -> .4    or   00 -> 0
+    	'''remove leading zeroes to left of decimal point, keeping at most 1 zero
+    	e.g. -03.05E+4 -> 305E+4    or    05 -> 5    or   0.4 -> .4    or   00 -> 0'''
     	parsed_value = re.sub(r'^0*(\d.*)$', r'\1', value)
-    	# remove leading zeroes to right of decimal point, plus any immediately to
-    	# the left of decimal point, if value < 1
-    	# e.g. .05E+4 -> 5E+4    or   0.01 -> .1
+    	'''remove leading zeroes to right of decimal point, plus any immediately to
+    	the left of decimal point, if value < 1
+    	e.g. .05E+4 -> 5E+4    or   0.01 -> .1'''
     	parsed_value = re.sub(r'^0*(\.)0*(\d+)$', r'\1\2', parsed_value)
-    	# remove trailing zeroes to right of integer w/ no decimal point
-    	# e.g. 100 -> 1   but   100. -> 100.
+    	'''remove trailing zeroes to right of integer w/ no decimal point
+    	e.g. 100 -> 1   but   100. -> 100.'''
     	parsed_value = re.sub(r'^([1-9]+)0*$', r'\1', parsed_value)
     	return( parsed_value )
+
+class rounding_methods(Enum):
+    proper = 'proper'   # use if values are in true scientific notation
