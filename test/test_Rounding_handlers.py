e4b714a2d46db50212f98ff18e1e047828c8a34c
ChrisGottsacker <atcgottsacker@gmail.com>
2016-08-17 14:21:01 -0500

    Revert "Simple test cases"
    This reverts commit 50b2d4930f3012a6c07e98599d3ff893be9d49fc.

--- test/test_Rounding_handlers.py
+++ test/test_Rounding_handlers.py
@@ -1,11 +1,5 @@
-from src.precise_value import (Rounding_handler,
-    Rounding_handler_keep_integral_zeroes, Rounding_handler_proper)
+from src.precise_value import Rounding_handler
 import unittest
 
-class test_Rounding_handler(unittest.TestCase):
-    def test_cannot_be_instantiated(self):
-        with self.assertRaises(TypeError):
-            Rounding_handler()
-
-class test_Rounding_handler_keep_integral_zeroes(unittest.TestCase):
-    
+class test__init__(unittest.TestCase):
+    def test():
