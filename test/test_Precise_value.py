50b2d4930f3012a6c07e98599d3ff893be9d49fc
ChrisGottsacker <atcgottsacker@gmail.com>
2016-08-17 14:16:16 -0500

    Simple test cases

--- test/test_Precise_value.py
+++ test/test_Precise_value.py
@@ -2,5 +2,9 @@ from src.precise_value import Precise_value
 import unittest
 
 class test__init__(unittest.TestCase):
-    def test():
+    def setup(self):
         
+
+    def test__init__(self):
+        value_1 = Precise_value('no_rounding', '3')
+        value_2 = Precise_value('no_rounding', '3')
