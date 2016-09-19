19c8b0f49865529ef907fc654aa6170532d47a00
ChrisGottsacker <atcgottsacker@gmail.com>
2016-09-19 15:16:35 -0500

    Delete redundant functions in get_rqtl.py
    Same functionality now contained in Rounded_value class

--- src/get_rqtl.py
+++ src/get_rqtl.py
@@ -415,29 +415,6 @@ class Strains(object):
 		individual.add(line)	# add line of data to Individual object
 		self.strains[strain].append(individual)
 
-def num_decimals(value):
-	'''Counts number of significant digits to right of decimal point'''
-
-def num_sigfigs(value):
-	'''
-	Counts number of significant figures in a numeric string.
-	TODO: replace regex w/ conditional logic (regex decreased efficiency by 10%)
-	'''
-	parsed_value = remove_non_digits()
-	# remove leading zeroes to left of decimal point, keeping at most 1 zero
-		# e.g. -03.05E+4 -> 305E+4    or    05 -> 5    or   0.4 -> .4    or   00 -> 0
-	parsed_value = re.sub(r'^0*(\d.*)$', r'\1', parsed_value)
-	# remove leading zeroes to right of decimal point, plus any immediately to
-	# the left of decimal point, if value < 1
-		# e.g. .05E+4 -> 5E+4    or   0.01 -> .1
-	parsed_value = re.sub(r'^0*(\.)0*(\d+)$', r'\1\2', parsed_value)
-	# remove trailing zeroes to right of integer w/ no decimal point
-		# e.g. 100 -> 1   but   100. -> 100.
-	parsed_value = re.sub(r'^([1-9]+)0*$', r'\1', parsed_value)
-	# remove decimal point
-	parsed_value = re.sub(r'\.', r'', parsed_value)
-	return( len(parsed_value) )
-
 def is_numeric(string):
 	try:
 		Decimal(string)
