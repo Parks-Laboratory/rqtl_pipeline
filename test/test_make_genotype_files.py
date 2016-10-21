7d4d3e7fbe6d1ea601adb3c6aa8716e889c93ab3
ChrisGottsacker <atcgottsacker@gmail.com>
2016-10-21 14:59:09 -0500

    Finished tests, bug exists
    If either test class is run alone, makeRQTLInputs.py builds

--- test/test_make_genotype_files.py
+++ test/test_make_genotype_files.py
@@ -1,7 +1,17 @@
 from src.makeRQTLInputs import *
 import unittest
 import io
+import pdb;
 from testfixtures import TempDirectory, tempdir, compare	# easier setup/cleanup test output
+
+def examine_dir(dir):
+	'''Pauses execution to allow examination of temporary directory'''
+	print('Temporary dir.:',dir.path)
+	print('Press "y" to continue execution.')
+	cmd = 'n'
+	while(cmd != 'y'):
+		cmd = input()
+
 
 geno_file = '''
 rs29270490
@@ -17,10 +27,10 @@ rs6196410
 '''
 
 
-
 pheno_file = '''Mouse ID,Strain,Sex,Weight-0 wks diet,AVG Food Intake - 4 Wks Diet,Trigly. ,Esterified Chol,Liver/NMR_Mass_8wks
 OB-642,BXD1/TyJ,Male,26.13,1.916,26,135,0.034937672
 OB-643,BXD1/TyJ,Male,24.3,1.916,,NA,0.032453926
+OB-644,BXD1/TyJ,Male,26.02,1.916,,NA,0.031792949
 OB-645,BXD1/TyJ,Female,17.5,1.796666667,,-51,0.037768186
 OB-646,BXD1/TyJ,Female,16.8,1.796666667,,-71,0.04374075
 OB-647,BXD1/TyJ,Female,21.03,1.796666667,21,119,0.038343416
@@ -32,10 +42,12 @@ OB-125,BXD27/TyJ,Female,20.76,3.367,45.3125,223.4375,0.03878796
 OB-126,BXD27/TyJ,Female,19.5,3.367,26.5625,159.375,0.031361702
 OB-127,BXD27/TyJ,Female,19.76,3.367,20.3125,118.75,0.030875435
 OB-638,BXD27/TyJ,Female,19.66,3.4,22,246,0.034066768
+OB-639,BXD27/TyJ,Female,21.76,3.4,28,254,0.04117155
+OB-640,BXD27/TyJ,Female,19.39,3.4,33,201,0.031396196
 OB-641,BXD27/TyJ,Female,17.51,3.4,21,196,0.031466593'''
 
 
-class geno_test(unittest.TestCase):
+class geno_test():
 	'''Set-up functionality required for test-classes in this file'''
 	def buildFiles(cls, use_average_by_strain):
 		dir = TempDirectory()
@@ -67,30 +79,31 @@ class geno_test(unittest.TestCase):
 
 		output = cls.dir.read(output_filename, 'utf-8')
 
+		# examine_dir(cls.dir)
+		# pdb.set_trace()
 		self.maxDiff = None  # display comparisons of all lines
 		self.assertMultiLineEqual(output, correct_output)
 
 
-@unittest.skip("not done")
+# @unittest.skip("not done")
 class test_make_genotype_files_average(geno_test):
+	''''''
 	@classmethod
 	def setUpClass(cls):
 		super().buildFiles(cls, True)
-	# def test_average(self):
-
 
 	def test_average(self):
-		correct_output = '''id,,,OB_642,OB_643,OB_645,OB_646,OB_647,OB_794,OB_795,OB_847,OB_848,OB_125,OB_126,OB_127,OB_638,OB_641
-rs6196410,1,33.7580975,A,A,A,A,A,A,A,A,A,B,B,B,B,B
-rs27034352,11,54.085187,B,B,B,B,B,A,A,A,A,A,A,A,A,A
-rs29588301,13,44.7742875,B,B,B,B,B,B,B,B,B,B,B,B,B,B
-rs29568258,13,45.250384,B,B,B,B,B,B,B,B,B,H,H,H,H,H
-rs50906391,16,22.8040365,A,A,A,A,A,B,B,B,B,B,B,B,B,B
-rs6262668,3,75.764573,B,B,B,B,B,B,B,B,B,B,B,B,B,B
-rs32595055,5,17.9862455,A,A,A,A,A,B,B,B,B,A,A,A,A,A
-rs31196255,7,57.8978535,A,A,A,A,A,A,A,A,A,A,A,A,A,A
-rs36773429,9,42.7108535,B,B,B,B,B,A,A,A,A,A,A,A,A,A
-rs29270490,X,66.918496,A,A,A,A,A,A,A,A,A,B,B,B,B,B
+		correct_output = '''id,,,BXD1_TyJ.f,BXD1_TyJ.m,BXD11_TyJ.f,BXD11_TyJ.m,BXD27_TyJ.f,BXD27_TyJ.m
+rs6196410,1,33.7580975,A,A,A,A,B,B
+rs27034352,11,54.085187,B,B,A,A,A,A
+rs29588301,13,44.7742875,B,B,B,B,B,B
+rs29568258,13,45.250384,B,B,B,B,H,H
+rs50906391,16,22.8040365,A,A,B,B,B,B
+rs6262668,3,75.764573,B,B,B,B,B,B
+rs32595055,5,17.9862455,A,A,B,B,A,A
+rs31196255,7,57.8978535,A,A,A,A,A,A
+rs36773429,9,42.7108535,B,B,A,A,A,A
+rs29270490,X,66.918496,A,A,A,A,B,B
 '''
 		super().compareFiles(type(self), correct_output)
 
@@ -98,25 +111,25 @@ rs29270490,X,66.918496,A,A,A,A,A,A,A,A,A,B,B,B,B,B
 	def tearDownClass(cls):
 		TempDirectory.cleanup_all()
 
+
+# @unittest.skip("not done")
 class test_make_genotype_files_not_average(geno_test):
 	@classmethod
 	def setUpClass(cls):
 		super().buildFiles(cls, False)
-	# def test_average(self):
-
 
 	def test_not_average(self):
-		correct_output = '''id,,,OB_642,OB_643,OB_645,OB_646,OB_647,OB_794,OB_795,OB_847,OB_848,OB_125,OB_126,OB_127,OB_638,OB_641
-rs6196410,1,33.7580975,A,A,A,A,A,A,A,A,A,B,B,B,B,B
-rs27034352,11,54.085187,B,B,B,B,B,A,A,A,A,A,A,A,A,A
-rs29588301,13,44.7742875,B,B,B,B,B,B,B,B,B,B,B,B,B,B
-rs29568258,13,45.250384,B,B,B,B,B,B,B,B,B,H,H,H,H,H
-rs50906391,16,22.8040365,A,A,A,A,A,B,B,B,B,B,B,B,B,B
-rs6262668,3,75.764573,B,B,B,B,B,B,B,B,B,B,B,B,B,B
-rs32595055,5,17.9862455,A,A,A,A,A,B,B,B,B,A,A,A,A,A
-rs31196255,7,57.8978535,A,A,A,A,A,A,A,A,A,A,A,A,A,A
-rs36773429,9,42.7108535,B,B,B,B,B,A,A,A,A,A,A,A,A,A
-rs29270490,X,66.918496,A,A,A,A,A,A,A,A,A,B,B,B,B,B
+		correct_output = '''id,,,OB_642,OB_643,OB_644,OB_645,OB_646,OB_647,OB_794,OB_795,OB_847,OB_848,OB_125,OB_126,OB_127,OB_638,OB_639,OB_640,OB_641
+rs6196410,1,33.7580975,A,A,A,A,A,A,A,A,A,A,B,B,B,B,B,B,B
+rs27034352,11,54.085187,B,B,B,B,B,B,A,A,A,A,A,A,A,A,A,A,A
+rs29588301,13,44.7742875,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B
+rs29568258,13,45.250384,B,B,B,B,B,B,B,B,B,B,H,H,H,H,H,H,H
+rs50906391,16,22.8040365,A,A,A,A,A,A,B,B,B,B,B,B,B,B,B,B,B
+rs6262668,3,75.764573,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B
+rs32595055,5,17.9862455,A,A,A,A,A,A,B,B,B,B,A,A,A,A,A,A,A
+rs31196255,7,57.8978535,A,A,A,A,A,A,A,A,A,A,A,A,A,A,A,A,A
+rs36773429,9,42.7108535,B,B,B,B,B,B,A,A,A,A,A,A,A,A,A,A,A
+rs29270490,X,66.918496,A,A,A,A,A,A,A,A,A,A,B,B,B,B,B,B,B
 '''
 		super().compareFiles(type(self), correct_output)
 
