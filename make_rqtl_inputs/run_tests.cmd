9478c803c68399238013a59cbd626da840d62471
ChrisGottsacker <atcgottsacker@gmail.com>
2016-11-02 13:02:15 -0500

    Fixed unittest calls

---
+++ make_rqtl_inputs/run_tests.cmd
@@ -0,0 +1,6 @@
+REM python -m unittest test/test_Individual_averaged.py
+REM python -m unittest test/test_Significant_value.py
+REM python -m unittest test/test_make_phenotype_files.py
+REM python -m unittest test/test_make_genotype_files.py
+cls
+python -m unittest discover test
