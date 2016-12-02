332273b211861e2aba179ce318b9eefcbdcbfff3
ChrisGottsacker <atcgottsacker@gmail.com>
2016-12-02 14:59:24 -0600

    Minor documentation changes

--- run_pipeline.cmd
+++ run_pipeline.cmd
@@ -1,27 +1,23 @@
 :: Filters genotype markers using PLINK and builds inputs for R/QTL
 :: See README_RUN_PIPELINE.md for more details
 
+:: DON'T CHANGE THIS
+SETLOCAL
+SET TRUE=0==0
+SET FALSE=NOT %TRUE%
 
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Temporarily add locations to the search path
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-	:: DON'T CHANGE THIS
-	SETLOCAL
-	SET TRUE=0==0
-	SET FALSE=NOT %TRUE%
+:: DON'T CHANGE THIS unless rqtl_pipeline directory is moved
+SET rqtl_pipeline=E:\rqtl_pipeline\
 
-	:: DON'T CHANGE THIS unless rqtl_pipeline directory is moved
-	SET %rqtl_pipeline%=E:\rqtl_pipeline\
+:: DON'T CHANGE THIS
+	:: add script locations to Windows' PATH variable, so cmd.exe can find them
+PATH %PATH%;%rqtl_pipeline%\;%rqtl_pipeline%\filter_markers\;%rqtl_pipeline%\make_rqtl_inputs\;%rqtl_pipeline%\make_rqtl_inputs\src\
 
-	:: DON'T CHANGE THIS
-		:: add script locations to Windows' PATH variable, so cmd.exe can find them
-	PATH %PATH%;%rqtl_pipeline%;%rqtl_pipeline%\filter_markers;%rqtl_pipeline%\make_rqtl_inputs;%rqtl_pipeline%\make_rqtl_inputs\src
+:: DON'T CHANGE THIS
+	:: specify location for all generated R/QTL input files, PLINK input/output files
+SET "OUTPUT_DIR=rqtl_%RANDOM%"
 
-	:: DON'T CHANGE THIS
-		:: specify location for all generated R/QTL input files, PLINK input/output files
-	SET "OUTPUT_DIR=rqtl_%RANDOM%"
-
-	cls
+cls
 
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -29,6 +25,7 @@
 ::		& Make phenotype file for R/QTL
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 	:: CHANGE THIS
+		:: assumes -avg is either used/not used for both R/QTL pheno/geno files
 	make_rqtl_inputs.py -out %OUTPUT_DIR% -avg pheno -phenoFile bxd.csv
 
 
@@ -42,6 +39,8 @@
 
 	:: CHANGE THIS
 		:: (NOTE: don't change anything that looks like  file/directory name)
+		:: file-prefix specified in --tfile must match file-prefix specified
+		:: in -out option of make_plink_inputs
 	plink --tfile %OUTPUT_DIR%/plink_input --make-bed --geno 0.1 --maf 0.05 --out %OUTPUT_DIR%/filtered_markers --write-snplist
 
 
@@ -50,6 +49,7 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 	:: CHANGE THIS
 		:: file-prefix specified in -mkFile must match file-prefix specified in --out option of PLINK
+		:: assumes -avg is either used/not used for both R/QTL pheno/geno files
 	make_rqtl_inputs.py -out %OUTPUT_DIR% -avg geno -mkFile %OUTPUT_DIR%/filtered_markers.snplist -table "[dbo].[rqtl_csvsr_geno_format]" -mkTable "[dbo].[genotypes]" -db HMDP
 
 
@@ -57,11 +57,12 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Make inputs for R/QTL Batch Mapping
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-	:: CHANGE THIS to TRUE if you want to map on CHTC cluster
 
-	:: DON'T CHANGE THIS
+	:: CHANGE THIS first line's condition to %TRUE% if you want to generate input file
+	:: for mapping on CHTC cluster
 	IF %TRUE% (
-		MOVE make_rdata.r %OUTPUT_DIR%
+		:: DON'T CHANGE THIS
+		copy make_rdata.r %OUTPUT_DIR%
 		SET prevDir=%cd%
 		cd %OUTPUT_DIR%
 		R CMD BATCH --no-save make_rdata.r
